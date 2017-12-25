4404cc1272bf6f188513f14b537e5f378001e8ba
Kyokoo <genesisrock91@gmail.com>
2017-12-25 14:41:51 +0100

    Sections & co

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
 
+
   var link = $('nav').find('a');
+  var navbarSize = $('nav').height() - 1;
 
   link.on('click', function(event) {
     event.preventDefault();
       hash = this.hash;
       if (this.hash !== ""){
         $('html, body').animate({
-          scrollTop: $(hash).offset().top
+          scrollTop: ($(hash).offset().top - navbarSize)
         }, 800, function () {
-          window.location.hash = hash;
+          window.location.hash = hash - navbarSize;
         });
       }
       else {
-
         $('html,body').animate({
           scrollTop: 0
         }, 800, function () {
@@ -21,4 +22,5 @@ $(document).ready(function() {
         });
       }
   });
+
 });
